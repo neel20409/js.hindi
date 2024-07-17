ef4f9923db9fde93f59dd29727ce892d61188211
Neel Bhatt <bhattneel2004@gmail.com>
2024-07-17 13:39:44 +0000

    datatype

---
+++ Basics/01_var.js
@@ -0,0 +1,7 @@
+const accountId =144553
+let accountEmail="Neel@gmail.com"
+var accountPassword="234"
+accountCity="Jaipur"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ Basics/02_datatype.js
@@ -0,0 +1,9 @@
+"use strict"
+// treat all javascript as newer verson
+// alert(3+5) we are using node js ,not browser
+let age=18
+let name="Neel"
+let loginID=true
+console.log([typeof(age),typeof(name)])
+console.log(typeof(undefine));//undefine datatype
+console.log(typeof(null));//object data type
