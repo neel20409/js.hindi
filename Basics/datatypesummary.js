efcce6fac33b75b1aee39069f55f8c54be3f501e
Neel Bhatt <bhattneel2004@gmail.com>
2024-07-18 12:05:20 +0000

    datatypesummary

---
+++ Basics/datatypesummary.js
@@ -0,0 +1,25 @@
+//primitive datatype(7)
+// String,number,null,boolean,undefined,symbol,BigInt
+
+// //dynamically typed language
+// const score = 100
+// const id = Symbol("abc");
+// const accid = Symbol("abc");
+// console.log(id === accid);
+
+
+//reference(non-primitive)
+// // Array,objects,functons
+
+// const heros = ["me","you","everyone"];
+// let info = {
+//     name:"Bhatt Neel",
+//     std:"10",
+//     age:"19yrs",
+// }
+// console.log(info);
+// const myfunction = function () {
+//     console.log("hello world ");
+    
+// }
+// console.log(myfunction);
