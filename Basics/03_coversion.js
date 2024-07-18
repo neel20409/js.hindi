6ab5fc041420d09c74fde069ebda81321480c4e2
Neel Bhatt <bhattneel2004@gmail.com>
2024-07-18 05:32:14 +0000

    conversion

---
+++ Basics/03_coversion.js
@@ -0,0 +1,22 @@
+// let score="33"
+
+// console.log(typeof(score));
+// console.log(typeof score);
+
+
+// let valuinnum = Number(score)
+// console.log(typeof valueinnum);
+
+// let isloggedin = ""
+
+// let booleaisloggedin = Boolean(isloggedin)
+// console.log(booleaisloggedin)
+//1=> true
+//0=>false
+// string=>true
+// empty=>false
+
+let num = 45
+let stringnum = String(num)
+console.log(typeof(stringnum));
+console.log(stringnum);
