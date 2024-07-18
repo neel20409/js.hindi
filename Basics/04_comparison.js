770c638380dae65bf868aaaa42fc2d978f54be3a
Neel Bhatt <bhattneel2004@gmail.com>
2024-07-18 11:40:09 +0000

    comparison operator

---
+++ Basics/04_comparison.js
@@ -0,0 +1,10 @@
+// console.log(3<=3);
+
+console.log("2">1);
+console.log("02">1);
+console.log(null>=0);
+console.log(undefined==0);//always false
+
+//strict check ===
+console.log("2"==2);//true
+console.log("2"===2);//false
