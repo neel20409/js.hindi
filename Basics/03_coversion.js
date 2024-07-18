be77836e58ac89322be33e4b04e5386edd3fe5d4
Neel Bhatt <bhattneel2004@gmail.com>
2024-07-18 05:52:23 +0000

    operation

--- Basics/03_coversion.js
+++ Basics/03_coversion.js
@@ -18,5 +18,35 @@
 
 let num = 45
 let stringnum = String(num)
-console.log(typeof(stringnum));
-console.log(stringnum);
+// console.log(typeof(stringnum));
+// console.log(stringnum);
+
+
+
+
+//*********************operation******************************
+
+let vaal =3
+let negvaal = -vaal
+// console.log(negvaal);
+// console.log(2+2);
+// console.log(2*4);
+// console.log(3-2);
+// console.log(4/2);
+// console.log(4%2);
+
+
+
+let str1 = "Hello"
+let str2= " Neel"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+2+3)
+// console.log(1+2+"3")
+
+let gamecounter = 200;
+gamecounter++;
+console.log(gamecounter);
+++gamecounter;
+console.log(gamecounter)
