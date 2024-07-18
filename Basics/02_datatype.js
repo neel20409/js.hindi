5e28fab503aaeb8064f729ae23f19ba94628a29e
Neel Bhatt <bhattneel2004@gmail.com>
2024-07-18 05:24:21 +0000

    conversion

--- Basics/02_datatype.js
+++ Basics/02_datatype.js
@@ -3,7 +3,8 @@
 // alert(3+5) we are using node js ,not browser
 let age=18
 let name="Neel"
-let loginID=true
+let loginID= true
 console.log([typeof(age),typeof(name)])
 console.log(typeof(undefine));//undefine datatype
 console.log(typeof(null));//object data type
+console.log(typeof(loginId));
