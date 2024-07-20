ee91d182889b33bdd88f0dbcfa8705e6d8755459
Neel Bhatt <bhattneel2004@gmail.com>
2024-07-20 12:39:48 +0000

    math&num

---
+++ Basics/06_nums&maths.js
@@ -0,0 +1,35 @@
+const score = 300
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString().length);
+// console.log(typeof(balance));
+
+const othnum = 23.45874
+// console.log(othnum.toPrecision(2));
+
+const hun = 1000000
+// console.log(hun.toLocaleString('en-IN'));
+
+
+// ++++++++++++++++++++ Math ++++++++++++++++++
+
+console.log(Math);
+// console.log(Math.abs(-20));//convert negative(-) into positve(+)
+// console.log(Math.round(4.456));//roundoff similar
+//     console.log(Math.ceil(4.23));
+    // console.log(Math.floor(4.23));
+// console.log(Math.sqrt(25));
+// console.log(Math.min(4,5,6,2,4));
+// console.log(Math.max(23,4553567,87965,34646));
+
+// console.log(Math.random());
+// console.log(Math.floor(Math.random()* 10 )+ 1);
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
