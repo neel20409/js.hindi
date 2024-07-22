977082650d6f4d4de775c8238e8b7b0942682d6f
Neel Bhatt <bhattneel2004@gmail.com>
2024-07-22 12:36:37 +0000

    function in object

--- 03_Basics/01_functons.js
+++ 03_Basics/01_functons.js
@@ -20,6 +20,24 @@ const result = add(5,8)//here we provide arguments
 function loginmsg(username = "Bhatt"){
     return(`${username} just logged in !!`)
 }
-console.log(loginmsg("Neel"));
-console.log(loginmsg());// no argument pass then undefined output or by default vale will be provided
+// console.log(loginmsg("Neel"));
+// console.log(loginmsg());// no argument pass then undefined output or by default vale will be provided
+
+function calccartprice(...num1) {
+    return num1
+    
+}
+// console.log(calccartprice(234,345,456));
+
+const user ={
+    name:"neel",
+    price: 655
+}
+
+function handleobj(anyobject){
+    console.log(`username is ${anyobject.name} and price is ${anyobject.price} buy give ratings!! `);
+}
+handleobj(user)
+
+
 
