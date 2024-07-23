bd77a29fc0d6cb25011c5e234a9d121a45300206
Neel Bhatt <bhattneel2004@gmail.com>
2024-07-23 04:03:00 +0000

    arrow

---
+++ 03_Basics/03_arrow.js
@@ -0,0 +1,34 @@
+const user = {
+    name : "neel",
+    price : 999,
+
+    welcomemsg: function(){
+    // console.log(`${this.name},welcome to website` );
+    // console.log(this);
+   }
+}
+// console.log(this);
+// user.welcomemsg()
+
+// function chai(){
+//     const name = "Neel"
+//     console.log(this.name);// can`t perfom in function only in object
+// }
+// chai()
+const chai =  () => {
+    const name = " neel"
+console.log(this);
+
+}
+// chai()
+
+//arrow function
+
+// const two = (num1,num2)=>{
+//     return num1+num2;
+
+// }
+// // console.log(two(45,34));
+const two = (num1,num2) =>  (num1+num2)//implicite return,
+
+console.log(two(23,34));
