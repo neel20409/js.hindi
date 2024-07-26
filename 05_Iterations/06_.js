73a57af728f27bbd766e9548f5a844624fd440f7
Neel Bhatt <bhattneel2004@gmail.com>
2024-07-26 13:10:57 +0000

    filter

---
+++ 05_Iterations/06_.js
@@ -0,0 +1,28 @@
+// const code =["js","python","java","c"]
+
+// const value = code.forEach((item)=>{
+//    // console.log(item);
+//     return item;
+// })
+// console.log(value);
+
+
+const nums =[1,2,3,4,5,6,7,8,9,10]
+// const update = nums.filter((num)=>{
+//    return  num>4
+// })
+// console.log(update);
+
+//same using for each
+
+// const news = []
+// nums.forEach((num) => {
+//     if(num>4){
+//         news.push(num)}
+    
+// });
+// console.log(news);
+
+
+ 
+
