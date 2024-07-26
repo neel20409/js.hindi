51779479edf2a0e3c02aab3fbe2d4c1f7ee5686a
Neel Bhatt <bhattneel2004@gmail.com>
2024-07-26 08:31:34 +0000

    while,do while

---
+++ 05_Iterations/02_.js
@@ -0,0 +1,20 @@
+// let index =0;
+
+// while (index<=10) {
+    // console.log(`even value ${index}`);
+    // index=index+2
+    
+// }
+
+
+let arr = ["Neel","Meet","Shiv"]
+let index =0;
+while(index<arr.length){
+    // console.log(`value of array ${arr[index]}`)
+    index=index+1
+}
+let score =11
+do {
+    console.log(`value of score ${score}`);
+    score++
+} while (score<=10);
