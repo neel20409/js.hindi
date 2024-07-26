b7d1b3984bb61720b0cf4fc349a9b6875527be1b
Neel Bhatt <bhattneel2004@gmail.com>
2024-07-26 09:12:33 +0000

    for in

---
+++ 05_Iterations/04_.js
@@ -0,0 +1,24 @@
+const obj1 ={
+    "js ":"javascript",
+    "cpp":"c++",
+    "ne":"neel",
+    "sh":"shiv",
+    "me":"meet"
+}
+//using for in loop for object iteration
+for (const key in obj1) {
+    //  console.log(`${key} is short cut for ${obj1[key]}`); 
+        
+    
+}
+
+
+//for in use in array
+const pro =["js",'phython',"c","c++","php","java"]
+for (const key in pro) {
+    // console.log(key);//only printing key or index
+}
+for (const key in pro) {
+    // console.log(pro[key]);
+}
+
