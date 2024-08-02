06b1c54f2bce31ed5e90990c08c4c6e14847b9a2
Neel Bhatt <bhattneel2004@gmail.com>
2024-08-02 12:01:20 +0000

    DOM

---
+++ 06_DOM/ApnaDOM/script.js
@@ -0,0 +1,18 @@
+// console.dir(document.body);
+// let heads = document.getElementById("heading");
+// console.dir(heads);
+let headss=document.getElementsByClassName("heading");
+// console.dir(headss);
+let tag = document.getElementsByTagName("p");
+// console.dir(tag);
+
+let firstelement = document.querySelector("p");
+// console.dir(firstelement);
+let allelement = document.querySelectorAll("p");
+// console.dir(allelement);
+let classs = document.querySelectorAll(".heading");
+// console.dir(classs);
+
+
+
+
