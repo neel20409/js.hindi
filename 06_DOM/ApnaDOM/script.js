e36ec9849677049eac7bcebe82b4607d18bd8673
Neel Bhatt <bhattneel2004@gmail.com>
2024-08-03 04:44:41 +0000

    DOM

--- 06_DOM/ApnaDOM/script.js
+++ 06_DOM/ApnaDOM/script.js
@@ -14,5 +14,29 @@ let classs = document.querySelectorAll(".heading");
 // console.dir(classs);
 
 
+let appendd = document.querySelector("h2")
+// console.dir(appendd.innerText);
+appendd.innerText=appendd.innerText+" from apna college"
 
+
+let box = document.querySelectorAll(".box");
+// console.log(box[0].innerText);
+// console.log(box[1].innerText);
+// console.log(box[2].innerText);
+// box[0].innerText=box.innerText + "All are the divs!!"
+// box[1].innerText=box.innerText + "All are the divs!!"
+// box[2].innerText=box.innerText + "All are the divs!!"
+
+let idx=0
+for(div of box){
+    div.innerText="new text";
+    idx++;
+}
+let newbtn = document.createElement("button");
+newbtn.innerText=("Click me!!")
+console.log(newbtn);
+// let divs = document.querySelectorAll("#box");
+document.body.append(newbtn);
+newbtn.style.color="white";
+newbtn.style.backgroundColor="black";
 
